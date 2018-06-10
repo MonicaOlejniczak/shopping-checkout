29a1874dba4db9d3be5071244fd54b8473910489
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2018-06-10 21:37:16 +1000

    Create a line item

---
+++ src/checkout/line-item.test.ts
@@ -0,0 +1,25 @@
+import { BigNumber } from "bignumber.js";
+import { Product } from "../product/product";
+import { LineItem } from "./line-item";
+
+describe("LineItem", () => {
+  const product = new Product({
+    name: "Super iPad",
+    price: new BigNumber("549.99"),
+    sku: "ipd",
+  });
+
+  it("does not allow a negative quantity", () => {
+    expect(() => new LineItem({ product, quantity: -1 })).toThrow("Quantity cannot be negative");
+  });
+
+  it("stores the product and quantity in the line", () => {
+    const lineItem = new LineItem({ product, quantity: 1 });
+    expect(lineItem).toMatchObject({ product, quantity: 1 });
+  });
+
+  it("calculates the line price based on the product price and quantity", () => {
+    const lineItem = new LineItem({ product, quantity: 3 });
+    expect(lineItem.linePrice).toEqual(new BigNumber("1649.97"));
+  });
+});
---
+++ src/checkout/line-item.ts
@@ -0,0 +1,37 @@
+import { BigNumber } from "bignumber.js";
+import { Product } from "../product/product";
+
+export type LineItemOpts = {
+  product: Product;
+  quantity: number;
+};
+
+/**
+ * A line item represents a single distinct set of products during checkout.
+ */
+export class LineItem {
+  /**
+   * The combined price of all the products in the line item.
+   */
+  public readonly linePrice: BigNumber;
+  public readonly product: Product;
+  public readonly quantity: number;
+
+  /**
+   * Create a line item.
+   *
+   * @param {LineItemOpts} opts The options that define a line item.
+   * @param {Product} opts.product The distinct product in the line item.
+   * @param {number} opts.quantity The amount of products in the line item. This value cannot be negative.
+   */
+  constructor(private opts: LineItemOpts) {
+    const { product, quantity } = this.opts;
+    if (quantity < 0) {
+      throw new Error("Quantity cannot be negative");
+    }
+
+    this.linePrice = product.price.times(quantity);
+    this.product = product;
+    this.quantity = quantity;
+  }
+}
